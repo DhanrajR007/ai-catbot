45b0b582ca559db2585a59c2299f5317d9558766
DhanrajR007 <dhanrajrathore1111@gmail.com>
2025-08-19 11:32:23 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const app = require("./src/app");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const generateResponse = require("./src/service/ai.service");
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+
+  socket.on("ai-message", async (data) => {
+    console.log(data.prompt)
+    const response = await generateResponse(data.prompt);
+    console.log(response);
+    socket.emit("ai-message-response",{response})
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
+
